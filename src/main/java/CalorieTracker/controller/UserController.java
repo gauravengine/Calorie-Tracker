28ad2586ab07c7729fa6f025bfc8364f05640278
Nightfury874 <jibaba874@gmail.com>
2023-10-12 15:21:23 +0530

    added PUT update user

--- src/main/java/CalorieTracker/controller/UserController.java
+++ src/main/java/CalorieTracker/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -23,4 +25,39 @@ public class UserController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> userData = userRepository.findById(id);
+
+        if (userData.isPresent()){
+            return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping ("/update/{id}")
+
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user){
+        Optional<User> userData = userRepository.findById(id);
+        if (userData.isPresent()){
+            User _user = userData.get();
+            _user.setUserName(user.getUserName());
+            _user.setDateOfBirth(user.getDateOfBirth());
+            _user.setHeight(user.getHeight());
+            _user.setWeight(user.getWeight());
+            _user.setProfileImgUrl(user.getProfileImgUrl());
+
+            return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
+
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+
 }
